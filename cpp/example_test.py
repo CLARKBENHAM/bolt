3a49c146e4b2d48fd0862a13ec7a361cd1098672
clark-hive <clark@thehive.ai>
2022-11-03 03:51:16 +0000

    run C++ tests via pybinding with correct name

--- cpp/example_test.py
+++ cpp/example_test.py
@@ -1,37 +1,71 @@
 import os
 import sys
 
-print('about to import', file=sys.stderr)
-print('python is', sys.version_info)
-print('pid is', os.getpid())
+#print('about to import', file=sys.stderr)
+#print('python is', sys.version_info)
+#print('pid is', os.getpid())
 
 #import mithral_wrapped
 from cpp import mithral_wrapped
 
-print('imported, about to call', file=sys.stderr)
+#print('imported, about to call', file=sys.stderr)
 
 #Test pybind11 wrapped correctly
 result =mithral_wrapped.add(2, 3)
-print(result)
 assert result == 5
-print(dir(mithral_wrapped), mithral_wrapped.add(mithral_wrapped.sub(2, 3), 3))
+#print(dir(mithral_wrapped), mithral_wrapped.add(mithral_wrapped.sub(2, 3), 3))
 
 #Run imported test cases
 my_str = "Caltech3x3"
 my_str_as_bytes = str.encode(my_str)
-print(type(my_str_as_bytes)) # ensure it is byte representation
-my_decoded_str = my_str_as_bytes.decode()
-print(type(my_decoded_str)) 
-name=my_str_as_bytes
-#name=bytearray("Caltech3x3", 'utf-8')
+name=my_str#_as_bytes #my_str_as_bytes
+##name=bytearray("Caltech3x3", 'utf-8')
+#print(type(my_str_as_bytes)) # ensure it is byte representation
+#my_decoded_str = my_str_as_bytes.decode()
+#print(type(my_decoded_str)) 
+#print('std::string', mithral_wrapped.utf8_test(name))
+#print('charptr', mithral_wrapped.utf8_charptr(name))
+
+C=mithral_wrapped.clark(name)
+print("getNameString", C.getNameString())
+print("getName", C.getName())
+print("Clark", C.c)
+print("Clark worked\n\n")
+
+
+N,D,M = (224 - 3 + 1) * (224 - 3 + 1), 3 * (3 * 3), 2
+T = mithral_wrapped.Test(N,D,M, name)
+print("TESTING: ", T.name)
+mithral_wrapped.printNameTest(T) #get this to work and pybinding work
+print(T.N, T.D, T.M)
+print(T, name)
+print("\n\n")
+
+print("MatmulTaskShape")
+N,D,M = (224 - 3 + 1) * (224 - 3 + 1), 3 * (3 * 3), 2
+matmul= mithral_wrapped.MatmulTaskShape(N,D,M, name)
+print("TESTING: ", matmul.name)
+mithral_wrapped.printNameMatmul(matmul) #get this to work and pybinding work
+print(matmul.N, matmul.D, matmul.M)
+print(matmul, name)
+print("\n\n")
+ncodebooks= [64] #[2, 4, 8, 16, 32, 64]
+lutconsts= [-1] #[-1, 1, 2, 4]
+#mithral_wrapped._profile_mithral(matmul, ncodebooks, lutconsts)
 
 N,D,M = (224 - 3 + 1) * (224 - 3 + 1), 3 * (3 * 3), 2
 kCaltechTaskShape0=mithral_wrapped.MatmulTaskShape(N,D,M, name)
-ncodebooks=[2, 4, 8, 16, 32, 64]
-lutconsts=[-1, 1, 2, 4]
+#kCaltechTaskShape0.name=name #if I assigned to it then code sometimes gets corrupted
 print(type(kCaltechTaskShape0))
-#print(kCaltechTaskShape0.name) #errors
-#print(kCaltechTaskShape0)
-out=mithral_wrapped._profile_mithral(kCaltechTaskShape0, ncodebooks, lutconsts)
+print('N: ', kCaltechTaskShape0.N) 
+print('D: ', kCaltechTaskShape0.D) 
+print('M: ', kCaltechTaskShape0.M) 
+print('name: ', kCaltechTaskShape0.name) #errors
+print(kCaltechTaskShape0, name)
+mithral_wrapped.printNameMatmul(kCaltechTaskShape0) #get this to work and pybinding work
+
+mithral_wrapped._profile_mithral(kCaltechTaskShape0, ncodebooks, lutconsts)
+##a bit faster
+mithral_wrapped._profile_mithral_int8(kCaltechTaskShape0, ncodebooks, lutconsts)
 
 print('done!', file=sys.stderr)
--- cpp/mithral_wrapped.cpp
+++ cpp/mithral_wrapped.cpp
@@ -4,7 +4,23 @@
 #include "src/quantize/mithral.hpp"
 #include "test/quantize/profile_amm.hpp"
 
+using namespace std;
+
+struct Test { int N, D, M; const char* name;};
+
 namespace py = pybind11;
+    
+    void printNameMatmul(const MatmulTaskShape& shape)
+    {
+        cout << shape.name << endl;
+        printf("shapename: %s\n", shape.name);
+    }
+    
+    void printNameTest(const Test& shape)
+    {
+        cout << "out:    " << shape.name << endl;
+        printf("shapename: %s\n", shape.name);
+    }
 
 PYBIND11_MODULE(mithral_wrapped, m) {
     m.doc() = "pybind11 plugin that wrapps mithral"; // Optional module docstring
@@ -14,27 +30,140 @@ PYBIND11_MODULE(mithral_wrapped, m) {
     m.def("sub", &sub, "A function that subs two numbers");
     
     //---profile_amm.hpp ---
-    //struct MatmulTaskShape { int N, D, M; const char* name; };
-    py::class_<MatmulTaskShape>(m, "MatmulTaskShape")
-       .def(py::init<int, int, int, const char*>())
-       //.def_read("name", &MatmulTaskShape::name)
-        .def("__repr__",
-        [](const MatmulTaskShape &a) {
-            return "<example.MatmulTaskShape: name: " + std::string(a.name) + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">";
-        }
-        );
-    //static constexpr MatmulTaskShape kCaltechTaskShape0 {
-    //  (224 - 3 + 1) * (224 - 3 + 1), 3 * (3 * 3), 2, "Caltech3x3"}; // 49284, 27
+    m.def("_profile_mithral", py::overload_cast<const MatmulTaskShape&, std::vector<int> , std::vector<float> >(&_profile_mithral), "generic C++ tests run");
+    m.def("_profile_mithral_int8", py::overload_cast<const MatmulTaskShape&, std::vector<int> , std::vector<float> >(&_profile_mithral<int8_t>), "generic C++ tests run in int8");
+    
+    
     //py::class_<MatmulTaskShape>(m, "MatmulTaskShape")
-    //   .def(py::init<int, int, int, const char*>())
-    //   //.def_read("name", &MatmulTaskShape::name)
+    //   .def(py::init([](int N, int D, int M, char *c){
+    //       const size_t len = strlen(c);
+    //       char * tmp_filename = new char[len + 1];
+    //       strncpy(tmp_filename, c, len);
+    //       tmp_filename[len] = '\0';  //nessisary line(why ?!?)
+    //       const char* c2 = tmp_filename;
+    //       return MatmulTaskShape{N,D,M,c2};
+    //   }))
+    //   .def_readwrite("name", &MatmulTaskShape::name) 
+    //   .def_readonly("N", &MatmulTaskShape::N) 
+    //   .def_readonly("D", &MatmulTaskShape::D) 
+    //   .def_readonly("M", &MatmulTaskShape::M) 
     //    .def("__repr__",
     //    [](const MatmulTaskShape &a) {
-    //        return "f<example.MatmulTaskShape: name: f{a.name} sizes: f{a.N}, f{a.D}, f{a.M}>";
+    //        std::cout << "PPPprinting:    " << a.name << std::endl; 
+    //        std::string name(a.name);
+    //        std::cout << std::string("from C++: <example.MatmulTaskShape: name: " + name + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">") << std::endl;
+    //        return std::string("<example.MatmulTaskShape: name: " + name + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">");
     //    }
     //    );
+    //
+    //.def(py::init([](int N, int D, int M, std::string s){ //Single comment changes code!!
     
-    //void _profile_mithral(const MatmulTaskShape& shape, std::vector<int> ncodebooks,
-    //                 std::vector<float> lut_work_consts)
-    m.def("_profile_mithral", py::overload_cast<const MatmulTaskShape&, std::vector<int> , std::vector<float> >(&_profile_mithral), "generic C++ tests run");
+    py::class_<MatmulTaskShape>(m, "MatmulTaskShape")
+       .def(py::init([](int N, int D, int M, char *c){
+           const size_t len = strlen(c);
+           char * tmp_filename = new char[len + 1];
+           strncpy(tmp_filename, c, len);
+           tmp_filename[len] = '\0';  //nessisary line(why ?!?)
+           const char* c2 = tmp_filename;
+           return MatmulTaskShape{N,D,M,c2};
+       }))
+       .def_readwrite("name", &MatmulTaskShape::name) 
+       .def_readonly("N", &MatmulTaskShape::N) 
+       .def_readonly("D", &MatmulTaskShape::D) 
+       .def_readonly("M", &MatmulTaskShape::M) 
+        .def("__repr__",
+        [](const MatmulTaskShape &a) {
+            std::cout << "PPPprinting:    " << a.name << std::endl; 
+            std::string name(a.name);
+            std::cout << std::string("from C++: <example.MatmulTaskShape: name: " + name + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">") << std::endl;
+            return std::string("<example.MatmulTaskShape: name: " + name + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">");
+        }
+        );
+
+    // TEMP
+    m.def("printNameMatmul", &printNameMatmul, "print matmul");
+    m.def("printNameTest", &printNameTest, "printTest");
+
+    // Hacking. Want to pass a python string to a struct which stores it in a const char * and returns a string python can read 
+    struct clark { 
+        clark(const char* c) : c{c}, s{c} {}; // w/ default constructor the std::string methods don't work
+        const char* getName() const { return c; }
+        const std::string getNameString() const { 
+            // std::string cast_c(c);
+            cout << "const char* " << c << endl;
+            cout << "std::string " << s << endl;
+            return s;
+            //return std::string (c); //Python can't decode this return
+        }
+        const char *c;
+        std::string s;
+    } ;
+
+    py::class_<clark>(m, "clark")
+       .def(py::init([](char *c){
+           //WORKS: makes a copy of pystring inside pybind. 
+           //Causes a memory leak of tmp_filename
+           const size_t len = strlen(c);
+           char * tmp_filename = new char[len + 1];
+           strncpy(tmp_filename, c, len);
+           tmp_filename[len] = '\0';  //nessisary line(why ?!?)
+           const char* c2 = tmp_filename;
+           return clark{c2};
+           
+        ////Doesn't work for s std::string
+        //std::string s2 = s;
+        //const char* c=s2.c_str();
+        //return clark{c};
+                       
+        //const char * c = s.c_str(); //errors
+        //char *c = new char[s.length() + 1];
+        
+        //std::string name(c);
+        //const char * c_ptr = name.c_str();
+        //cout << "making: " << c_ptr <<endl;
+        //return clark{c_ptr};
+       }))
+        .def("getName", &clark::getName)
+        .def("getNameString", &clark::getNameString)
+       .def_readwrite("c", &clark::c) ;
+    
+    py::class_<Test>(m, "Test")
+       .def(py::init([](int N, int D, int M, char *c){
+           const size_t len = strlen(c);
+           char * tmp_filename = new char[len + 1];
+           strncpy(tmp_filename, c, len);
+           tmp_filename[len] = '\0';  //nessisary line(why ?!?)
+           const char* c2 = tmp_filename;
+           return Test{N,D,M,c2};
+       }))
+       .def_readwrite("name", &Test::name) 
+       .def_readonly("N", &Test::N) 
+       .def_readonly("D", &Test::D) 
+       .def_readonly("M", &Test::M) 
+        .def("__repr__",
+        [](const Test &a) {
+            std::cout << "PPPprinting:    " << a.name << std::endl; 
+            std::string name(a.name);
+            std::cout << std::string("from C++: <Test: name: " + name + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">") << std::endl;
+            return std::string("<Test: name: " + name + " sizes: " + std::to_string(a.N) + " ," + std::to_string(a.D) + ", " + std::to_string(a.M) + ">");
+        }
+        );
+
+    // can print out fine
+    m.def("utf8_test",
+    [](const std::string s) {
+        const char * c = s.c_str();
+        cout << "utf-8 is icing on the cake.\n";
+        cout << c << "\n";
+        return c;
+    }
+    );
+    m.def("utf8_charptr",
+        [](const char *s) {
+            cout << "My favorite food is\n";
+            cout << s << "\n";
+            return s; //Python can print returned char* 
+        }
+    );
+    
 }
