4a35b2958b1964210390905f5be49b6c1e0db720
clark-hive <clark@thehive.ai>
2022-12-01 01:21:37 +0000

    more verbose cpp/__init__.py

--- cpp/__init__.py
+++ cpp/__init__.py
@@ -1,7 +1,12 @@
 import os 
 os.chdir(os.path.dirname(__file__))
+
+MITHRAL_BUILD="./bazel-bin/mithral_wrapped.so"
+if not os.path.isfile(MITHRAL_BUILD):
+  raise Exception(f"Expected file {MITHRAL_BUILD} Was `bazel build :mithral_wrapped` run?")
+
 try:
-	os.symlink("./bazel-bin/mithral_wrapped.so", "mithral_wrapped.so")
+	os.symlink(MITHRAL_BUILD, "mithral_wrapped.so")
 except FileExistsError:
   pass
 
