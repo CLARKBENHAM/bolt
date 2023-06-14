cf021c470d8988ad6259f4b0d395ba866b451e22
clark-hive <clark@thehive.ai>
2023-06-14 12:08:07 -0700

    also loop over ncodebooks. Lut comments fixed

--- experiments/graph_mithral.py
+++ experiments/graph_mithral.py
@@ -45,86 +45,88 @@ MetricsSoftmax = namedtuple("MetricsSoftmax", ["np_time", "py_fit_time", "py_est
 #Run on last layers of NN
 results=[]
 results_std=[]
-ncodebooks=2
 NREPS=10
 NAVG=5
-print(f"ncodebooks={ncodebooks}")
 for data in itertools.chain(*data_sources):
   print("$$$$$data", data.name)
-  min_trials = []
-  for _ in range(NAVG):
-    trials=[]
-    for _ in range(NREPS):
-      [W_test,W_train, X_test, X_train, Y_test, Y_train] = attrgetter('W_test','W_train', 'X_test', 'X_train', 'Y_test', 'Y_train')(data)
-      #Mithral C++ doesn't work with counts not aligned to 32
-      align32=len(Y_test)-(len(Y_test)%32)
-      Y_test=Y_test[:align32]
-      X_test=X_test[:align32]
-      lutconsts=-1
-      t = time.perf_counter()
-      Y=X_test@W_test
-      np_time=time.perf_counter() - t
-      #mse=np.mean((np.abs(np.ravel(Y) - np.ravel(Y_test)))**2)
-      #assert mse < 0.001*Y.size, mse
-      max_ix=np.apply_along_axis(np.argmax, 1, Y_test)
-      
-      hparams_dict = {'ncodebooks': ncodebooks, 'lut_work_const': lutconsts}
-      est = vq_amm.MithralMatmul(**hparams_dict)
-      t = time.perf_counter()
-      est.fit(X_train,W_train)
-      py_fit_time=time.perf_counter() - t
- 
-      t = time.perf_counter()
-      Y_hat1 = est.predict(X_test, W_test)
-      py_est_time=time.perf_counter() - t
-      py_est_r2 = r2_score(Y,Y_hat1)
-      py_max_ix=np.apply_along_axis(np.argmax, 1, Y_hat1)
-      py_est_per_ix_kept=np.sum(py_max_ix==max_ix)/py_max_ix.size
-      
-      task=mithral_wrapped.mithral_amm_task_float(*X_test.shape,W_test.shape[1], ncodebooks, lutconsts)
-      task.amm.out_mat = np.zeros(task.amm.out_mat.shape)
-      t = time.perf_counter()
-      task.X=X_test
-      task.Q=W_test
-      copy_python_to_amm(est, task.amm)
-      copy_python_luts(est, task.amm) # Or can make luts in C++
-      copy_to_cpp_time=time.perf_counter() - t
-
-      #task.X=X_train[:len(X_test)]
-      #task.lut()#use X known at train time
-      #task.X =X_test
-      t = time.perf_counter()
-      task.run_matmul(False)
-      #task.run_matmul(True) #Encodes test X as centroids instead of using train_x's centroids
-      Y_hat2=task.amm.out_mat #Since we just care about relative order for predicting output
-      cpp_est_time=time.perf_counter() - t
-      Y_hat2=(Y_hat2.astype(np.uint16)*ncodebooks/task.amm.out_scale) + task.amm.out_offset_sum
-      cpp_est_r2=r2_score(Y, Y_hat2)
-      cpp_max_ix=np.apply_along_axis(np.argmax, 1, Y_hat2)
-      cpp_est_per_ix_kept=np.sum(cpp_max_ix==max_ix)/cpp_max_ix.size
-      o= MetricsSoftmax(np_time, py_fit_time, py_est_time, py_est_r2, py_est_per_ix_kept, copy_to_cpp_time, cpp_est_time, cpp_est_r2, cpp_est_per_ix_kept)
-      
-      trials += [o]
-    min_trials += [MetricsSoftmax(*np.min(trials, axis=0))]
-  print(f"##Each Min Trial of {NREPS} for {data.name}##")
-  attr=['np_time', 'py_est_r2', 'py_est_per_ix_kept', 'cpp_est_time', 'cpp_est_r2', 'cpp_est_per_ix_kept']
-  print(attr)
-  for o in min_trials:
-    print(attrgetter(*attr)(o))
-    
-  results += [MetricsSoftmax(*np.average(min_trials, axis=0))]
-  results_std += [MetricsSoftmax(*np.std(min_trials, axis=0))]
+  for ncodebooks in [16]:
+    print(f"ncodebooks={ncodebooks}")
+    min_trials = []
+    for _ in range(NAVG):
+      trials=[]
+      for _ in range(NREPS):
+        [W_test,W_train, X_test, X_train, Y_test, Y_train] = attrgetter('W_test','W_train', 'X_test', 'X_train', 'Y_test', 'Y_train')(data)
+        #Mithral C++ doesn't work with counts not aligned to 32
+        align32=len(Y_test)-(len(Y_test)%32)
+        Y_test=Y_test[:align32]
+        X_test=X_test[:align32]
+        lutconsts=-1
+        t = time.perf_counter()
+        Y=X_test@W_test
+        np_time=time.perf_counter() - t
+        #mse=np.mean((np.abs(np.ravel(Y) - np.ravel(Y_test)))**2)
+        #assert mse < 0.001*Y.size, mse
+        max_ix=np.apply_along_axis(np.argmax, 1, Y_test)
+        
+        hparams_dict = {'ncodebooks': ncodebooks, 'lut_work_const': lutconsts}
+        est = vq_amm.MithralMatmul(**hparams_dict)
+        t = time.perf_counter()
+        est.fit(X_train,W_train)
+        py_fit_time=time.perf_counter() - t
+   
+        t = time.perf_counter()
+        Y_hat1 = est.predict(X_test, W_test)
+        py_est_time=time.perf_counter() - t
+        py_est_r2 = r2_score(Y,Y_hat1)
+        py_max_ix=np.apply_along_axis(np.argmax, 1, Y_hat1)
+        py_est_per_ix_kept=np.sum(py_max_ix==max_ix)/py_max_ix.size
+        
+        task=mithral_wrapped.mithral_amm_task_float(*X_test.shape,W_test.shape[1], ncodebooks, lutconsts)
+        task.amm.out_mat = np.zeros(task.amm.out_mat.shape)
+        t = time.perf_counter()
+        copy_python_to_amm(est, task.amm)
+        copy_python_luts(est, task.amm) # Or can make luts in C++
+        task.X=X_test
+        task.Q=W_test
+        copy_to_cpp_time=time.perf_counter() - t
   
-min_np_times=list(map(lambda i: i.np_time, results))
-min_mithral_times=list(map(lambda i: i.cpp_est_time, results))
-cpp_est_per_ix_kepts=list(map(lambda i: i.cpp_est_per_ix_kept, results))
-print(f"ncodebooks={ncodebooks}")
-print(f"Avg over {NAVG} of Min of {NREPS} Numpy Matrix mult times: {min_np_times}, {[i.np_time for i in results_std]}")
-print(f"Avg over {NAVG} of Min of {NREPS} Mithral times: {min_mithral_times}, {[i.cpp_est_time for i in results_std]}")
-print(f"Avg over {NAVG} of Min of {NREPS} cpp_est_per_ix_keps: {cpp_est_per_ix_kepts}")
-print(f"Py/C++ time ratio: {[i/j for i,j in zip(min_np_times, min_mithral_times)]}")
+        #task.Q=W_train[:len(W_test)]  # W_train and W_test the same here; not needed
+        #task.lut() #making luts in C++ and Py has R^2 of 0.999 
+        #task.Q =W_test
+        t = time.perf_counter()
+        task.run_matmul(False)
+        #task.run_matmul(True) #Encodes test Q as LUT instead of using train_Q's luts 
+        Y_hat2=task.amm.out_mat #Since we just care about relative order for predicting output
+        cpp_est_time=time.perf_counter() - t
+        Y_hat2=(Y_hat2.astype(np.uint16)*task.amm.ncodebooks/task.amm.out_scale) + task.amm.out_offset_sum
+        cpp_est_r2=r2_score(Y, Y_hat2)
+        cpp_max_ix=np.apply_along_axis(np.argmax, 1, Y_hat2)
+        cpp_est_per_ix_kept=np.sum(cpp_max_ix==max_ix)/cpp_max_ix.size
+        o= MetricsSoftmax(np_time, py_fit_time, py_est_time, py_est_r2, py_est_per_ix_kept, copy_to_cpp_time, cpp_est_time, cpp_est_r2, cpp_est_per_ix_kept)
+
+        trials += [o]
+
+      min_trials += [MetricsSoftmax(*np.min(trials, axis=0))]
+    print(f"## Min of {NREPS} trials for {data.name}##")
+    attr=['np_time', 'py_est_r2', 'py_est_per_ix_kept', 'cpp_est_time', 'cpp_est_r2', 'cpp_est_per_ix_kept']
+    print(attr)
+    for o in min_trials:
+      print(attrgetter(*attr)(o))
+      
+    results += [MetricsSoftmax(*np.average(min_trials, axis=0))]
+    results_std += [MetricsSoftmax(*np.std(min_trials, axis=0))]
+    
+  min_np_times=list(map(lambda i: i.np_time, results))
+  min_mithral_times=list(map(lambda i: i.cpp_est_time, results))
+  cpp_est_per_ix_kepts=list(map(lambda i: i.cpp_est_per_ix_kept, results))
+  print(f"ncodebooks={ncodebooks}")
+  print(f"Avg over {NAVG} of Min of {NREPS} Numpy Matrix mult times: {min_np_times}, {[i.np_time for i in results_std]}")
+  print(f"Avg over {NAVG} of Min of {NREPS} Mithral times: {min_mithral_times}, {[i.cpp_est_time for i in results_std]}")
+  print(f"Avg over {NAVG} of Min of {NREPS} cpp_est_per_ix_keps: {cpp_est_per_ix_kepts}")
+  print(f"Py/C++ time ratio: {[i/j for i,j in zip(min_np_times, min_mithral_times)]}")
 
 #%%
+quit()
 plt.title("Raw Y")
 plt.hist(Y_test.flatten(),bins=30,label='Y',alpha=0.3)
 plt.hist(Y_hat1.flatten(),bins=30,label='Y_hat1 (py)', alpha=0.3)
